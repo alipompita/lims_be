9bd2efe99ef40475d311ed8c8bcbc571738a35de
alipompita <58941738+alipompita@users.noreply.github.com>
2026-03-03 12:26:22 +0200

    remove unused import on SampleReceptionCOntroller

--- app/Http/Controllers/Api/SampleReceptionController.php
+++ app/Http/Controllers/Api/SampleReceptionController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\SampleReceipt;
-use App\Models\User;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 
