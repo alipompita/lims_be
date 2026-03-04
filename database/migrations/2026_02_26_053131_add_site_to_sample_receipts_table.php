542acbfe4f16e3bd9baa742aa0f65e3099bb2249
alipompita <58941738+alipompita@users.noreply.github.com>
2026-03-04 11:30:57 +0200

    make site_id nullable on sample receipts

--- database/migrations/2026_02_26_053131_add_site_to_sample_receipts_table.php
+++ database/migrations/2026_02_26_053131_add_site_to_sample_receipts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('sample_receipts', function (Blueprint $table) {
-            $table->unsignedBigInteger('site_id');
+            $table->unsignedBigInteger('site_id')->nullable();
             $table->foreign('site_id')->references('id')->on('sites')->onDelete('cascade');
         });
     }
